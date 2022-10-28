72e9c49f2cc262815bc512dab807f152524b8231
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-10-28 17:23:55 -0300

    section brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './brand.css'
 
+import { google, react, dropbox, angular, microsoft } from './imports'
+
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='modern__brand section__padding'>
+      <div>
+        <img src={google} alt="Google logo" />
+      </div>
+      <div>
+        <img src={react} alt="React logo" />
+      </div>
+      <div>
+        <img src={dropbox} alt="Dropbox logo" />
+      </div>
+      <div>
+        <img src={angular} alt="Angular logo" />
+      </div>
+      <div>
+        <img src={microsoft} alt="Microsoft logo" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,9 @@
+import google from '../../assets/google.png'
+import react from '../../assets/react.png'
+import dropbox from '../../assets/dropbox.png'
+import angular from '../../assets/angular.png'
+import microsoft from '../../assets/microsoft.png'
+
+export {
+    google, react, dropbox, angular, microsoft
+}
