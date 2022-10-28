8bd9099a0cdb368ac4bb96d2d505b3c5396acc91
devmarcioreis <107413382+devmarcioreis@users.noreply.github.com>
2022-10-28 13:59:20 -0300

    header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css'
+import ai from '../../assets/face.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='modern__header section__padding' id='home'>
+      <div className="modern__header-content">
+        <h1 className='gradient__text'>Vamos construir algo moderno e incrivel</h1>
+        <p>
+        Tenho paixão por tecnologia, e ela me faz ter vontade de trabalhar e estudar cada vez mais para solucionar os mais variados tipos de problemas na área de desenvolvimento.</p>
+        <div className="modern__header-content__input">
+          <input type="email" placeholder='Seu email' />
+          <button type='button'>Enviar</button>
+        </div>
+        <div className="modern__header-image">
+          <img src={ai} alt="Face img" />
+        </div>
+      </div>
+    </div>
   )
 }
 
